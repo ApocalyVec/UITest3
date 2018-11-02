9069df1053034d108b582f97ff3c138fe0e5993a
Ziheng (Leo) Li <s-vector.lee@hotmail.com>
2018-11-02 00:13:39 -0400

    update all

--- app/src/main/java/com/apocalyvec/uitest3/MainActivity.java
+++ app/src/main/java/com/apocalyvec/uitest3/MainActivity.java
@@ -1,13 +1,19 @@
 package com.apocalyvec.uitest3;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.MessageFormat;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
 
     @Override
@@ -19,11 +25,34 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         ActionBarDrawerToggle toogle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toogle);
 
         toogle.syncState();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch(menuItem.getItemId()) {
+            case R.id.nav_store:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new StoreFragment()).commit();
+                break;
+            case R.id.nav_profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ProfileFragment()).commit();
+                break;
+            case R.id.nav_help:
+                Toast.makeText(this, "Help Yourself...", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_about:
+                Toast.makeText(this, "ECE2799 B18 Team 9", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        
+
+        return true;
     }
 
     @Override
