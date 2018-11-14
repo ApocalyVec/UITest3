3c333d7ff5625a236986c28016c634274408fe3e
ApocalyVec <zli12@wpi.edu>
2018-11-14 02:23:52 -0500

    add display children

--- app/src/main/java/com/apocalyvec/sleepandsound/HomeFragment.java
+++ app/src/main/java/com/apocalyvec/sleepandsound/HomeFragment.java
@@ -8,15 +8,54 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
+
 public class HomeFragment extends Fragment {
+
+    private DatabaseReference mDatabaseUserChildren;
+    private LinearLayout childrenList;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        //put in the card view for children
+
+        // connect to Firebase
+        mDatabaseUserChildren = FirebaseDatabase.getInstance().getReference().child("Users").child("User_1").child("Children").child("Child_1").child("Age");
+
 
         return inflater.inflate(com.apocalyvec.sleepandsound.R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // connect to GUI
+        childrenList = getActivity().findViewById(R.id.childrenList);
+
+        //put in the card view for children
+        mDatabaseUserChildren.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String FirstName = dataSnapshot.getValue().toString();
+                TextView name1 = getActivity().findViewById(R.id.childName);
+                name1.setText("Name: " + FirstName);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
