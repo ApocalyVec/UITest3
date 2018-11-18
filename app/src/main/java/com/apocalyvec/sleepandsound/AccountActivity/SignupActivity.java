ff980aa064b5c70e1e72a56bdf77c5d65c4168d9
ApocalyVec <zli12@wpi.edu>
2018-11-18 01:02:47 -0500

    add login dummies

---
+++ app/src/main/java/com/apocalyvec/sleepandsound/AccountActivity/SignupActivity.java
@@ -0,0 +1,15 @@
+package com.apocalyvec.sleepandsound.AccountActivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.apocalyvec.sleepandsound.R;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
